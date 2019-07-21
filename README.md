cd1646a0f27aa39907ca31779b58c4d34a640e0b
mangocoinproject <52526923+mangocoinproject@users.noreply.github.com>
2019-07-21 15:05:15 -0700

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# mangocoin-wallet-electron
-GUI wallet for Mangocoin
+## WalletShell - GUI wallet for Mangocoin.
+
+### Features:
+
+This wallet contains the basic functions required to manage your Mangocoin assets:
+
+* Wallet creation:
+  * Create new wallet.
+  * Import/recover from private keys OR mnemonic seed.
+* Basic wallet operation/transactions:
+  * Open an existing  wallet
+  * Display wallet address & balance
+  * Display & Backup private keys/seed
+  * Sending/transferring. Integrated Address or Payment ID are supported. Also provides address lookup from your addressbook.
+  * Transactions history listing/sorting/searching/detail.
+  * Incoming transaction notification.
+  * Export incoming, outgoing, or all transactions to csv file.
+  * Rescan wallet from specific block height.
+  * Perform wallet optimization.
+  * Utilities: generate payment id and integrated address.
+* Address book:
+  * Add/Edit/Delete address entry.
+  * Listing/sorting/searching existing entries.
+  * Allow to store same wallet address with different payment id.
+  * Autosave address after sending to new/unknown recipient
+  * Allow to optionally create password protected address book.
+* Misc:
+  * Option to use system tray (on closing/minimizing wallet)
+  * Allow to add custom node address.
+  * Theme: Dark & Light Mode
+  * [Keyboard shortcuts](docs/shortcut.md)
+
+### Download &amp; Run WalletShell
+
+#### Windows:
+1. Download the latest installer here: https://github.com/mangocoinproject/mangocoin-wallet-electron/releases/latest
+2. Run the installer (`mangocoin-<version>-win-setup.exe`) and follow the installation wizard.
+3. Launch Mangocoin via start menu or desktop shortcut.
+
+#### GNU/Linux (AppImage):
+1. Download latest AppImage bundle here: https://github.com/mangocoinproject/mangocoin-wallet-electron/releases/latest
+2. Make it executable, either via GUI file manager or command line, e.g. `chmod +x mangocoin-<version>-linux.AppImage`
+3. Run/execute the file, double click in file manager, or run via shell/command line (See: https://docs.appimage.org/user-guide/run-appimages.html)
+
+#### macOS
+1. Download latest archive here: https://github.com/mangocoinproject/mangocoin-wallet-electron/releases/latest
+2. Extract downloaded zip archived
+3. Run the executable binary (`Mangocoin.app/Contents/MacOs/WalletShell`)
+
+### Using WalletShell
+Please visit our wiki page: [WalletShell User Guide](../../wiki).
+
+### Building/Packaging WalletShell
+You need to have `Node.js` and `npm` installed, go to https://nodejs.org and find out how to get it installed on your platform.
+
+Once you have Node+npm installed:
+```
+# assuming you're building it on GNU/Linux
+# first, download turtle-service binary for each platform
+# from Mangocoin official repo
+# https://github.com/mangocoinproject/mangocoin/releases
+# extract the turtle-service executable somewhere
+
+# clone the repo
+$ git clone https://github.com/mangocoinproject/mangocoin-wallet-electron
+$ cd mangocoin-wallet-electron
+
+# install dependencies
+$ npm install
+
+# create build+dist directory
+$ mkdir -p ./build && mkdir -p ./dist
+
+# copy/symlink icons from assets, required for packaging
+$ cp ./src/assets/icon.* ./build/
+
+# build GNU/Linux package
+$ mkdir -p ./bin/lin
+$ cp /path/to/linux-version-of/mangocoin-service ./bin/lin/
+$ npm run dist-lin
+
+# build Windows package (you need to have wine 2.0+ installed)
+$ mkdir -p ./bin/win
+$ cp /path/to/win-version-of/mangocoin-service.exe ./bin/win/
+$ npm run dist-win
+
+# build OSX package
+$ mkdir -p ./bin/osx
+$ cp /path/to/osx-version-of/mangocoin-service ./bin/osx/
+$ npm run dist-mac
+```
+
+Resulting packages or installer can be found inside `dist/` directory.
---
+++ src/js/ws_config.js
@@ -0,0 +1,97 @@
+var config = {};
+
+// self explanatory, your application name, descriptions, etc
+config.appName = 'Mangocoin';
+config.appDescription = 'Mangocoin Wallet';
+config.appSlogan = 'Fast and private!';
+config.appId = 'lol.mangocoin.walletshell';
+config.appGitRepo = 'https://github.com/mangocoinproject/mangocoin-wallet-electron';
+
+// default port number for your daemon (e.g. TurtleCoind)
+config.daemonDefaultRpcPort = 11898;
+
+// wallet file created by this app will have this extension
+config.walletFileDefaultExt = 'mng';
+
+// change this to match your wallet service executable filename
+config.walletServiceBinaryFilename = 'mangocoin-service';
+
+// version on the bundled service (turtle-service)
+config.walletServiceBinaryVersion = "v0.13.0";
+
+// config file format supported by wallet service, possible values:
+// ini -->  for turtle service (or its forks) version <= v0.8.3
+// json --> for turtle service (or its forks) version >= v0.8.4
+config.walletServiceConfigFormat = "json";
+
+// default port number for your wallet service (e.g. turtle-service)
+config.walletServiceRpcPort = 8070;
+
+// block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
+config.blockExplorerUrl = 'http://explorer.mangocoin.online/?hash=[[TX_HASH]]#blockchain_transaction';
+
+// default remote node to connect to, set this to a known reliable node for 'just works' user experience
+config.remoteNodeDefaultHost = '3.18.105.111';
+
+
+// remote node list update url, set to null if you don't have one
+// for TRTL:
+// raw list: https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json
+// filtered: https://trtl.nodes.pub/api/getNodes
+config.remoteNodeListUpdateUrl = '#';
+
+// set to false if using raw/unfiltered node list
+config.remoteNodeListFiltered = true;
+
+// fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
+config.remoteNodeListFallback = [
+  '3.15.32.95:11898',
+  '13.59.178.203:11898',
+];
+config.remoteNodeCacheSupported = false;
+config.remoteNodeSslSupported = false;
+
+// your currency name
+config.assetName = 'Mangocoin';
+// your currency ticker
+config.assetTicker = 'MNG';
+// your currency address prefix, for address validation
+config.addressPrefix = 'M';
+// standard wallet address length, for address validation
+config.addressLength = 95;
+// integrated wallet address length, for address validation. Added length is length of payment ID encoded in base58.
+config.integratedAddressLength = config.addressLength + ((64 * 11) / 8);
+
+// minimum fee for sending transaction
+config.minimumFee = 0.1;
+// minimum amount for sending transaction
+config.mininumSend = 0.1;
+// default mixin/anonimity for transaction
+config.defaultMixin = 3;
+// to represent human readable value
+config.decimalPlaces = 8;
+// to convert from atomic unit
+config.decimalDivisor = 10 ** config.decimalPlaces;
+
+// obfuscate address book entries, set to false if you want to save it in plain json file.
+// not for security because the encryption key is attached here
+config.addressBookObfuscateEntries = true;
+// key use to obfuscate address book contents
+config.addressBookObfuscationKey = '79009fb00ca1b7130832a42de45142cf6c4b7f333fe6fba5';
+// initial/sample entries to fill new address book
+config.addressBookSampleEntries = [
+  {
+    name: 'WalletShell Donation',
+    address: 'MEpLh1LswBqihtwVB7VuYAQP7E39SYSwVQwFVyAjpGd6fdALVvZk74YTq5jTo4DNnTdkw2wcWCzJ2EtVJ9k9DhioBWQ7GGq',
+    paymentId: '',
+  }
+];
+// cipher config for private address book
+config.addressBookCipherConfig = {
+  algorithm: 'aes-256-gcm',
+  saltLenght: 128,
+  pbkdf2Rounds: 10000,
+  pbkdf2Digest: 'sha512'
+};
+
+module.exports = config;
